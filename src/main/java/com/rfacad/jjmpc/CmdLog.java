5a7f8b4bfc92717bede0022e41b78670d8962f9e
glreno <46288004+glreno@users.noreply.github.com>
2018-12-31 18:52:29 -0500

    OldCommit:664cc5791b29758ea7129f71a035c8e4bda76c19 OldCommit:5d5ade6365696731914ad41941232824f7f1ffd7 OldCommit:186c08779266714503eeb117307ca1a5ecd9ad53 OldCommit:d1963321a722519d11237c4287a8ec18c948e898 Date:Fri May 4 19:21:59 2018 -0400 Msg:eclipse ignores Msg:add log4j Msg:initial junit test Msg:initial pom file

--- src/main/java/com/rfacad/jjmpc/CmdLog.java
+++ src/main/java/com/rfacad/jjmpc/CmdLog.java
@@ -1,8 +1,13 @@
 package com.rfacad.jjmpc;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 @com.rfacad.Copyright("Copyright (c) 2018 Gerald Reno, Jr. All rights reserved. Licensed under Apache License 2.0")
 public class CmdLog implements ButtonCommand
 {
+	private static final Logger log = LogManager.getLogger(CmdLog.class);
+
 	private String msg;
 	private boolean logstate;
 	public CmdLog() { msg=null;logstate=true;}
@@ -10,16 +15,20 @@ public class CmdLog implements ButtonCommand
 	public CmdLog(String s,boolean state) { msg=s;logstate=state;}
 	public boolean button(BState state)
 	{
-		if ( logstate )
+		if ( state == null && msg == null )
 		{
-			System.out.println("ID:"+Integer.toHexString(state.getButtonId())
+			msg = "null state passed to CmdLog";
+		}
+		if ( logstate && state != null )
+		{
+			log.info("ID:"+Integer.toHexString(state.getButtonId())
 			+" Prev:"+state.getPrevValue()
 			+" Value:"+state.getNewValue()
 			+" Shifts:"+state.get(ButtonMapper.SHIFT));
 		}
 		if ( msg != null )
 		{
-			System.out.println(msg);
+			log.info(msg);
 		}
 		return true;
 	}
---
+++ src/test/java/com/rfacad/jjmpc/TestCmdLog.java
@@ -0,0 +1,22 @@
+package com.rfacad.jjmpc;
+
+import org.junit.Test;
+
+@com.rfacad.Copyright("Copyright (c) 2018 Gerald Reno, Jr. All rights reserved. Licensed under Apache License 2.0")
+public class TestCmdLog
+{
+	@Test
+	public void shouldHandleNullState() {
+		CmdLog cmd1 = new CmdLog();
+		cmd1.button(null);
+
+		CmdLog cmd2 = new CmdLog("Message,logstate=false but no state");
+		cmd2.button(null);
+
+		CmdLog cmd3 = new CmdLog("Message,logstate=true but no state",true);
+		cmd3.button(null);
+
+		CmdLog cmd4 = new CmdLog(null,false);
+		cmd4.button(null);
+	}
+}
